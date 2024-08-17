5dea0809acf271260d55ba118d2eeb2c3262aee4
milivojsa <milivojsa96@gmail.com>
2024-08-17 10:34:34 +0200

    Add submit endpoint and validation

---
+++ app/Http/Controllers/SubmitController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SubmitRequest;
+
+class SubmitController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(SubmitRequest $request)
+    {
+
+    }
+}
